060619164943359ce93dbe5c78a29380796c55dc
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2022-05-13 09:20:05 +0000

    noetic: explictly set python2 for shebang, find . -iname '*.py' -exec sed -i 's@^\(#!/usr/bin/env\)\s*python@\1 python2@' {} \;

--- utils/rtc-link/RtmTreeCtrl.py
+++ utils/rtc-link/RtmTreeCtrl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 #
 #  @file RtmTreeCtrl.py
 #  @brief rtc-link name tree management class
--- utils/rtc-template/vcproj_gen.py
+++ utils/rtc-template/vcproj_gen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- Python -*-
 #
 #  @file vcproj_gen.py
