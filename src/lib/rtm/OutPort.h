69ae66d194110644110acde86e433e23fe81459d
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2024-04-09 22:51:29 +0900

    Rebase from 'debian/noetic/openrtm_aist'

--- src/lib/rtm/OutPort.h
+++ src/lib/rtm/OutPort.h
@@ -138,8 +138,12 @@ namespace RTC
 #endif
         m_value(value), m_onWrite(0), m_onWriteConvert(0)
     {
-      addProperty("dataport.data_value", m_value);
-      m_propValueIndex = NVUtil::find_index(m_profile.properties, "dataport.data_value");
+      addProperty("dataport.data_value", CORBA::Short(0));
+      {
+	Guard guard(m_profile_mutex);
+	m_propValueIndex = NVUtil::find_index(m_profile.properties,
+					      "dataport.data_value");
+      }
     }
     
     /*!
@@ -211,7 +215,10 @@ namespace RTC
           (*m_onWrite)(value);
           RTC_TRACE(("OnWrite called"));
         }
-      m_profile.properties[m_propValueIndex].value <<= value;
+      {
+	Guard guard(m_profile_mutex);
+	m_profile.properties[m_propValueIndex].value <<= value;
+      }
 
       bool result(true);
       std::vector<const char *> disconnect_ids;
